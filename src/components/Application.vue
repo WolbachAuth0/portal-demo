35e66773f2209560ccc78fe299e4541e25b421f9
WolbachAuth0 <aaron.wolbach@okta.com>
2022-06-26 13:57:57 -0400

    add application component

---
+++ src/components/Application.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <v-avatar>
+        <img :src="logo_uri" :alt="name" />
+      </v-avatar>
+      {{ name }}
+    </v-card-title>
+
+    <v-card-actions>
+      <v-btn class="primary" :href="deployment_url">Login</v-btn>
+      <v-btn class="secondary" @click="toggleShowJSON(clientID)">
+        {{ showJSON ? `Hide App Data` : `Show App Data` }}
+      </v-btn>
+    </v-card-actions>
+
+    <v-divider></v-divider>
+
+    <v-card-text v-if="showJSON">
+      <pre class="json">
+        {{ JSON.stringify(data) | pretty }}
+      </pre>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Application',
+  data() {
+    return {
+      showJSON: false,
+      app_type: 'app_type',
+      tenant: 'tenant-name',
+      name: 'app-name',
+      logo_uri: 'logo-uri',
+      deployment_url: '#',
+      data: {}
+    }
+  },
+  props: {
+    clientID: { type: String, required: true }
+  },
+  filters: {
+    pretty: function(value) {
+      return JSON.stringify(JSON.parse(value), null, 2);
+    }
+  },
+  async beforeMount () {
+    const response = await this.getClient()
+    if (response.status == 200) { 
+      this.data = response.data
+      this.app_type = response.data.app_type
+      this.tenant = response.data.tenant
+      this.name = response.data.name
+      this.logo_uri = response.data.logo_uri
+      this.deployment_url = response.data.client_metadata.deployment_url
+    }
+  },
+  methods: {
+    async getClient () {
+      const accesstoken = await this.$auth.getTokenSilently()
+      const response = await this.$http(accesstoken).get(`/clients/${this.clientID}`)
+      return response.data
+    },
+    toggleShowJSON() {
+      this.showJSON = !this.showJSON
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+  pre.json {
+    font-family: monospace, monospace;
+    display: block;
+    overflow: scroll;
+    padding: 0.5em;
+    color: white;
+    background: #282a36;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,57 @@
 <template>
-  <v-card>
+  <v-card class="mx-auto">
     <v-card-title>
       {{ appTitle }}
     </v-card-title>
 
+    <v-card-text v-if="!$auth.isAuthenticated">
+      Login into the portal app to access your third party applications.
+    </v-card-text>
+    <v-card-subtitle v-else>
+      <v-avatar>
+        <img :src="$auth.user.picture" :alt="$auth.user.name">
+      </v-avatar>
+      Welcome {{ $auth.user.name }}!
+    </v-card-subtitle>
+
+    <v-container fluid v-if="$auth.isAuthenticated">
+      <v-row dense>
+        <v-col v-for="clientID of clientIDs" :key="clientID" cols="6">
+          <application :clientID="clientID"></application>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-card>
 </template>
 
 <script>
+import Application from '../components/Application.vue'
+
 export default {
   name: 'Home',
   metaInfo: {
-    title: 'Welcome',
+    title: 'Home'
   },
-  data () {
+  components: {
+		Application
+	},
+  data() {
     return {
       appTitle: process.env.VUE_APP_TITLE,
-      tab: null,
-      selectedOrg: 0,
-      organizations: []
     }
   },
-  async change () {
-    if (this.$auth.isAuthenticated) {
-      const accesstoken  = await this.$auth.getTokenSilently()
-      console.log(accesstoken)
+  computed: {
+    clientIDs () {
+      if (this.$auth.isAuthenticated) {
+        // return list of client ID's the user is permitted to use.
+        const claim = `https://auth0-portal-demo.herokuapp.com/allowed-apps`
+        return this.$auth.user[claim]
+      } else {
+        return []
+      }
     }
   },
   methods: {
-   
   }
 }
 </script>
