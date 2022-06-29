2a9c8cc0ba388727c2f1aed7b0feb8d0dfdb98f2
WolbachAuth0 <aaron.wolbach@okta.com>
2022-06-29 15:20:47 -0400

    change colors / styles

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,11 +4,21 @@ import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
+const oktaBlue = '#07297a'
+const lightBlue = '#007DC1' // '#6d88c7'
+const defaults = {
+  primary: '#1976D2',
+  secondary: '#424242',
+  accent: '#82B1FF',
+  error: '#FF5252',
+  info: '#2196F3',
+  success: '#4CAF50',
+  warning: '#FFC107',
+}
 export default new Vuetify({
   theme: {
     dark: false,
-    default: true,
-    light: false,
+    light: true,
     themes: {
       dark: {
         background: colors.grey.darken2,
@@ -20,26 +30,19 @@ export default new Vuetify({
         success: colors.green.darken1,
         warning: colors.yellow.lighten4,
         error: colors.red.lighten4,
+        greyLighten4: colors.grey.lighten4
       },
       light: {
         background: colors.grey.lighten5,
         surface: colors.grey.lighten4,
-        // primary: colors.red.darken1, // #E53935
-        // secondary: colors.red.lighten4, // #FFCDD2
-        // accent: colors.indigo.base,
+        primary: oktaBlue,
+        secondary: lightBlue,
+        accent: colors.blue.base,
+        lightgrey: colors.grey.lighten2
         // info: colors.blueGrey.lighten3,
         // success: colors.green.lighten1,
         // warning: colors.yellow,
         // error: colors.red.darken3,
-      },
-      default: {
-        primary: '#1976D2',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107',
       }
     }
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,105 +3,111 @@
     <v-card-title>
       {{ appTitle }}
     </v-card-title>
-    <div v-if="$auth.isAuthenticated">
-      <v-card-subtitle>
-        <v-avatar>
-          <img :src="$auth.user.picture" :alt="$auth.user.name">
-        </v-avatar>
-        <h3>Welcome {{ $auth.user.nickname }}!</h3>
-      </v-card-subtitle>
-      <v-card-text>
-        User ({{ $auth.user.email}}) has access to the following applications.
-      </v-card-text>
-    </div>
-    <div v-else>
-      <v-card-subtitle >
-        <h3>Introduction</h3>
-      </v-card-subtitle>
 
-      <v-card-text>
-        Welcome to the Okta | Auth0 Portal Demonstration application. The purpose of this application is to 
-        demonstrate the single sign-on (SSO) capability of the Auth0 platform. This application behaves as gateway
-        for three other independent applications. When a user logs into this portal app, he or she will be presented
-        with the applications that have been assigned to the user. For each application there will be a "Login" 
-        button which allows the current user to navigate to those apps without being prompted for re-authentication. 
-      </v-card-text>
+    <v-sheet class="blue lighten-5">
+    
+      <div v-if="$auth.isAuthenticated">
+        <v-card-subtitle>
+          <v-avatar>
+            <img :src="$auth.user.picture" :alt="$auth.user.name">
+          </v-avatar>
+          <h3>Welcome {{ $auth.user.nickname }}!</h3>
+        </v-card-subtitle>
+        <v-card-text>
+          User ({{ $auth.user.email}}) has access to the following applications;
+        </v-card-text>
+      </div>
+      <div v-else>
+        <v-card-subtitle >
+          <h3>Introduction</h3>
+        </v-card-subtitle>
 
-      <v-card-text>
-        A description of the applications and users which are set up for this portal is below. After you have read
-        through these docs, login to this portal to try it out.
-      </v-card-text>
+        <v-card-text>
+          Welcome to the Okta | Auth0 Portal Demonstration application. The purpose of this application is to 
+          demonstrate the single sign-on (SSO) capability of the Auth0 platform. This application behaves as gateway
+          for three other independent applications. When a user logs into this portal app, he or she will be presented
+          with the applications that have been assigned to the user. For each application there will be a "Login" 
+          button which allows the current user to navigate to those apps without being prompted for re-authentication. 
+        </v-card-text>
 
-      <v-card-subtitle>
-        <h3>Applications</h3>
-      </v-card-subtitle>
+        <v-card-text>
+          A description of the applications and users which are set up for this portal is below. After you have read
+          through these docs, login to this portal to try it out.
+        </v-card-text>
 
-      <v-card-text>
-        There are three applications which this portal app behaves as a gateway. These apps are primarily examples
-        just to demonstrate logging in and logging out. With the exception of the Salesforce SSO application, the 
-        apps don't do anything dangerous. The Salesforce SSO application is here to demonstrate the capability of
-        Auth0 to behave as a SAML identity provider (IdP) into third party SaaS applications. Please note that each
-        of these applications is deployed on a free-tier of Heroku. As such, it may take a moment for them to spin up
-        if they haven't been accessed in the last 20 minutes.
-      </v-card-text>
+        <v-card-subtitle>
+          <h3>Applications</h3>
+        </v-card-subtitle>
 
-      <v-list-item two-line v-for="app in applications" :key="app.name">
-        <v-list-item-avatar>
-						<img :src="app.logo" :alt="app.name">
-					</v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title>{{ app.name }}</v-list-item-title>
-          <v-list-item-subtitle>
-            {{ app.description }}
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
+        <v-card-text>
+          There are three applications which this portal app behaves as a gateway. These apps are primarily examples
+          just to demonstrate logging in and logging out. With the exception of the Salesforce SSO application, the 
+          apps don't do anything dangerous. The Salesforce SSO application is here to demonstrate the capability of
+          Auth0 to behave as a SAML identity provider (IdP) into third party SaaS applications. Please note that each
+          of these applications is deployed on a free-tier of Heroku. As such, it may take a moment for them to spin up
+          if they haven't been accessed in the last 20 minutes.
+        </v-card-text>
 
-      <v-card-subtitle>
-        <h3>Users</h3>
-      </v-card-subtitle>
+        <v-list-item two-line v-for="app in applications" :key="app.name">
+          <v-list-item-avatar>
+              <img :src="app.logo" :alt="app.name">
+            </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>{{ app.name }}</v-list-item-title>
+            <v-list-item-subtitle>
+              {{ app.description }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
 
-      <v-card-text>
-        There are four users provisioned to this portal. The table below describes the applications each user
-        has been provisioned to. To try this out, ask your Okta Account Representative for the credentials for
-        each user.
-      </v-card-text>
+        <v-card-subtitle>
+          <h3>Users</h3>
+        </v-card-subtitle>
 
-      <v-simple-table>
-        <template v-slot:default>
-          <thead>
-            <tr>
-              <th class="text-left">
-                User
-              </th>
-              <th class="text-left">
-                Applications
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="user in users" :key="user.email">
-              <td>{{ user.email }}</td>
-              <td>{{ user.apps.join(', ') }}</td>
-            </tr>
-          </tbody>
-        </template>
-      </v-simple-table>
+        <v-card-text>
+          There are four users provisioned to this portal. The table below describes the applications each user
+          has been provisioned to. To try this out, ask your Okta Account Representative for the credentials for
+          each user.
+        </v-card-text>
+
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">
+                  User
+                </th>
+                <th class="text-left">
+                  Applications
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="user in users" :key="user.email">
+                <td>{{ user.email }}</td>
+                <td>{{ user.apps.join(', ') }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
 
 
-      <v-card-text>
-        In order to access a user's assigned applications, you must first login to the portal application.
-      </v-card-text>
-    </div>
+        <v-card-text>
+          In order to access a user's assigned applications, you must first login to the portal application.
+        </v-card-text>
+      </div>
 
-    <v-container fluid v-if="$auth.isAuthenticated">
-      <v-row dense>
-        <v-col v-for="clientID of clientIDs" :key="clientID" cols="6">
-          <application :clientID="clientID"></application>
-        </v-col>
-      </v-row>
-    </v-container>
+      <v-container fluid v-if="$auth.isAuthenticated">
+        <v-row dense>
+          <v-col v-for="clientID of clientIDs" :key="clientID" cols="6">
+            <application :clientID="clientID"></application>
+          </v-col>
+        </v-row>
+      </v-container>
+
+    </v-sheet>
   </v-card>
+
 </template>
 
 <script>
